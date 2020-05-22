5776d80653382291aab57bb5b3924034151e61ad
MarkusTryban <markustryban@gmail.com>
2020-05-22 19:03:22 -0400

    Add content to DrumDisplay render

--- src/DrumDisplay.js
+++ src/DrumDisplay.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 
 class DrumDisplay extends Component {
   constructor(props) {
-  super(props)
-  this.state = {
-    display: ''
+    super(props);
+    this.state = {
+      display: '',
+    };
   }
-}
 
   render() {
-    return ();
+    return (
+      <div id='drum-machine'>
+        <div id='display'>
+          <h1>Drum Machine</h1>
+        </div>
+      </div>
+    );
   }
 }
-
 
 export default DrumDisplay;
