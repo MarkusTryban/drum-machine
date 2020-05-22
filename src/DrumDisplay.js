e4dcf9becaed5ff6fd64c96cd0e37348a6ca1c78
MarkusTryban <markustryban@gmail.com>
2020-05-22 17:57:03 -0400

    Create DrumDisplay component

---
+++ src/DrumDisplay.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class DrumDisplay extends Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+    display: ''
+  }
+}
+
+  render() {
+    return ();
+  }
+}
+
+
+export default DrumDisplay;
