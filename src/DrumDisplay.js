93f13b128fac01040861772de8e0bdb9352061a6
MarkusTryban <markustryban@gmail.com>
2020-06-01 21:34:25 -0400

    Update drum display

--- src/DrumDisplay.js
+++ src/DrumDisplay.js
@@ -1,42 +1,97 @@
 import React, { Component } from 'react';
 
-import drumPadClip from './DrumPadClip';
+import DrumPad from './DrumPad';
+
+const bankOne = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Chord-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3',
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Chord-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3',
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Chord-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3',
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Shaker',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3',
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3',
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: 'Punchy-Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3',
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Side-Stick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3',
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Snare',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3',
+  },
+];
 
 export default class DrumDisplay extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      audioData: [],
+      displayText: this.props.displayTextDefault,
     };
-    this.GET_AUDIO_CLIP = drumPadClip();
+
+    this.updateDisplayText = this.updateDisplayText.bind(this);
   }
 
-  getAudio = () => {
-    fetch(this.GET_AUDIO_CLIP)
-      .then(res => res.json())
-      .then(result => {
-        this.setState({
-          audioData: result,
-        });
-      });
+  updateDisplayText = text => {
+    this.setState({
+      displayText: text,
+    });
   };
 
   render() {
     return (
-      <div id='drum-machine' className='drums'>
+      <div className='drums' id='drum-machine'>
         <div id='display'>
-          <h1>Drum Machine</h1>
+          <h1>{this.state.displayText}</h1>
         </div>
         <div className='drum-pad-layout'>
-          <button className='drum-pad'>Q</button>
-          <button className='drum-pad'>W</button>
-          <button className='drum-pad'>E</button>
-          <button className='drum-pad'>A</button>
-          <button className='drum-pad'>S</button>
-          <button className='drum-pad'>D</button>
-          <button className='drum-pad'>Z</button>
-          <button className='drum-pad'>X</button>
-          <button className='drum-pad'>C</button>
+          {bankOne.map((item, idx) => {
+            return (
+              <DrumPad
+                key={idx}
+                padItem={item}
+                updateDisplayText={this.updateDisplayText}
+              />
+            );
+          })}
         </div>
         <div className='footer'>
           by{' '}
