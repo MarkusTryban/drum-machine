1805b6a70f1b1a97d7f3be7fb19b38bc6a6fb58c
MarkusTryban <markustryban@gmail.com>
2020-06-03 12:55:35 -0400

    Play audio on key press

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -7,6 +7,15 @@ export default class DrumPad extends Component {
     this.audioHandler = React.createRef();
   }
 
+  onKeyDown = e => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const drum = document.querySelector(`.drum-pad[data-key="${e.keyCode}"]`);
+
+    if (!audio) return;
+
+    this.drumPadOnClick();
+  };
+
   drumPadOnClick = () => {
     const text = this.props.padItem.id;
     const audioElm = this.audioHandler.current;
@@ -15,11 +24,20 @@ export default class DrumPad extends Component {
     audioElm.play();
   };
 
+  componentDidMount() {
+    document.addEventListener('keydown', this.onKeyDown);
+  }
+
   render() {
     const padItem = this.props.padItem;
 
     return (
-      <div className='drum-pad' id={padItem.id} onClick={this.drumPadOnClick}>
+      <div
+        data-key={padItem.keyCode}
+        className='drum-pad'
+        id={padItem.id}
+        onClick={this.drumPadOnClick}
+      >
         <audio
           className='clip'
           id={padItem.keyTrigger}
