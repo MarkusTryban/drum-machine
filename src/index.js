81a1210e5600d2eded6954701372e5593b0a7361
MarkusTryban <markustryban@gmail.com>
2020-09-21 11:18:47 -0400

    Replace App with DrumDisplay

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import DrumDisplay from './DrumDisplay';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<DrumDisplay />, document.getElementById('root'));
