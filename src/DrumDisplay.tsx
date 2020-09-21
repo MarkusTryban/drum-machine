dbdff946565933c639776d767bfb34845516974a
MarkusTryban <markustryban@gmail.com>
2020-09-21 11:28:52 -0400

    Change class to functional component with hooks

--- src/DrumDisplay.tsx
+++ src/DrumDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import { drumPadArray } from './DrumPadArray';
 
@@ -8,51 +8,41 @@ import './DrumDisplay.css';
 
 const bankOne = drumPadArray();
 
-export default class DrumDisplay extends Component {
-  constructor(props) {
-    super(props);
+const DrumDisplay = () => {
+  const [displayText, setDisplayText] = useState('Drum Machine');
 
-    this.state = {
-      displayText: 'Drum Machine',
-    };
-
-    this.updateDisplayText = this.updateDisplayText.bind(this);
-  }
-
-  updateDisplayText = (text) => {
-    this.setState({
-      displayText: text,
-    });
+  const updateDisplayText = (text) => {
+    setDisplayText(text);
   };
 
-  render() {
-    return (
-      <div className='drums' id='drum-machine'>
-        <div id='display'>
-          <h1>{this.state.displayText}</h1>
-        </div>
-        <div className='drum-pad-layout'>
-          {bankOne.map((item, idx) => {
-            return (
-              <DrumPad
-                key={idx}
-                padItem={item}
-                updateDisplayText={this.updateDisplayText}
-              />
-            );
-          })}
-        </div>
-        <div className='footer'>
-          by{' '}
-          <a
-            href='https://markustryban.com'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Markus Tryban
-          </a>
-        </div>
+  return (
+    <div className='drums' id='drum-machine'>
+      <div id='display'>
+        <h1>{displayText}</h1>
       </div>
-    );
-  }
-}
+      <div className='drum-pad-layout'>
+        {bankOne.map((item, idx) => {
+          return (
+            <DrumPad
+              key={idx}
+              padItem={item}
+              updateDisplayText={updateDisplayText}
+            />
+          );
+        })}
+      </div>
+      <div className='footer'>
+        by{' '}
+        <a
+          href='https://markustryban.com'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Markus Tryban
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default DrumDisplay;
