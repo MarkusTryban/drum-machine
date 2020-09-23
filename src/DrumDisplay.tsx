82c7c923c6ea33f8cf160540ff3fbb43145149e3
MarkusTryban <markustryban@gmail.com>
2020-09-22 21:41:01 -0400

    Add drums-container

--- src/DrumDisplay.tsx
+++ src/DrumDisplay.tsx
@@ -16,32 +16,36 @@ const DrumDisplay = () => {
   };
 
   return (
-    <div className='drums' id='drum-machine'>
-      <div id='display'>
-        <h1>{displayText}</h1>
+    <>
+      <div className='drums-container'>
+        <div className='drums' id='drum-machine'>
+          <div id='display'>
+            <h1>{displayText}</h1>
+          </div>
+          <div className='drum-pad-layout'>
+            {bankOne.map((item, idx) => {
+              return (
+                <DrumPad
+                  key={idx}
+                  padItem={item}
+                  updateDisplayText={updateDisplayText}
+                />
+              );
+            })}
+          </div>
+          <div className='footer'>
+            by{' '}
+            <a
+              href='https://markustryban.com'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Markus Tryban
+            </a>
+          </div>
+        </div>
       </div>
-      <div className='drum-pad-layout'>
-        {bankOne.map((item, idx) => {
-          return (
-            <DrumPad
-              key={idx}
-              padItem={item}
-              updateDisplayText={updateDisplayText}
-            />
-          );
-        })}
-      </div>
-      <div className='footer'>
-        by{' '}
-        <a
-          href='https://markustryban.com'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Markus Tryban
-        </a>
-      </div>
-    </div>
+    </>
   );
 };
 
