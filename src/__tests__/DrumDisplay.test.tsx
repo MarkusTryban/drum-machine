10e1a1852e90db53ba8811ee534ae063ee4ae96e
MarkusTryban <markustryban@gmail.com>
2020-09-29 14:39:02 -0400

    Should return link

--- src/__tests__/DrumDisplay.test.tsx
+++ src/__tests__/DrumDisplay.test.tsx
@@ -7,3 +7,7 @@ import DrumDisplay from '../DrumDisplay';
 test('should render without crashing', () => {
   render(<DrumDisplay />);
 });
+test('should return link', () => {
+  render(<DrumDisplay />);
+  expect(screen.getByRole('link', { name: /markus tryban/i }));
+});
